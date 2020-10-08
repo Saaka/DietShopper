568e641b1517aced730cb2aa8f2e8bc04128c055
Saaka <saka1988@gmail.com>
2020-10-08 22:30:13 +0200

    Added missing method to IGoogleAuthorizationClient.

--- Src/DietShopper.Application/Auth/Services/IGoogleAuthorizationClient.cs
+++ Src/DietShopper.Application/Auth/Services/IGoogleAuthorizationClient.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using DietShopper.Application.Auth.Models;
+
 namespace DietShopper.Application.Auth.Services
 {
     public interface IGoogleAuthorizationClient
     {
-        
+        Task<AuthorizationData> AuthorizeWithGoogleToken(string googleToken);
     }
 }
