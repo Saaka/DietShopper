6cfa0b6a6bd1a057b4d9dd6177697bf8a5fdf1b8
Saaka <saka1988@gmail.com>
2020-10-15 22:31:37 +0200

    Updated User entity tests.

--- Src/Tests/DietShopper.Domain.UnitTests/Entities/UserTests.cs
+++ Src/Tests/DietShopper.Domain.UnitTests/Entities/UserTests.cs
@@ -11,13 +11,29 @@ namespace DietShopper.Domain.UnitTests.Entities
         public void Should_Have_Values_Set_In_Constructor()
         {
             var userGuid = Guid.NewGuid();
-            var user = new User(userGuid, "Thomas Anderson", "http://test.com/image", true);
+            var user = new User(userGuid, "Thomas Anderson", "neo@gmail.com", "http://test.com/image", false);
 
             user.UserId.Should().Be(0);
             user.UserGuid.Should().Be(userGuid);
             user.DisplayName.Should().Be("Thomas Anderson");
             user.ImageUrl.Should().Be("http://test.com/image");
+            user.IsAdmin.Should().BeFalse();
+            user.Email.Should().Be("neo@gmail.com");
+        }
+        
+        [Fact]
+        public void Should_Have_Values_Set_After_Update()
+        {
+            var userGuid = Guid.NewGuid();
+            var user = new User(userGuid, "Thomas Anderson", "neo@gmail.com", "http://test.com/image", false);
+            user.Update("Neo", "http://test.com/image2", true);
+
+            user.UserId.Should().Be(0);
+            user.UserGuid.Should().Be(userGuid);
+            user.DisplayName.Should().Be("Neo");
+            user.ImageUrl.Should().Be("http://test.com/image2");
             user.IsAdmin.Should().BeTrue();
+            user.Email.Should().Be("neo@gmail.com");
         }
     }
 }
