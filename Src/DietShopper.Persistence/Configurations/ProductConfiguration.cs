c718ca8eac2294df19fc3cfb7dfc127dc07d9678
Saaka <saka1988@gmail.com>
2020-10-17 17:23:39 +0200

    Added ProductConfiguration file.

---
+++ Src/DietShopper.Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,58 @@
+using DietShopper.Domain.Constants.Validation;
+using DietShopper.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DietShopper.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.ProductId);
+
+            builder.Property(x => x.ProductGuid)
+                .IsRequired();
+
+            builder.HasIndex(x => x.ProductGuid)
+                .HasName("IX_Products_ProductGuid")
+                .IncludeProperties(x => x.ProductId)
+                .IsUnique();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(ProductValidationConstants.ProductNameMaxLength);
+
+            builder.Property(x => x.ShortName)
+                .IsRequired()
+                .HasMaxLength(ProductValidationConstants.ProductShortNameMaxLength);
+
+            builder.Property(x => x.ShortName)
+                .IsRequired()
+                .HasMaxLength(ProductValidationConstants.ProductDescriptionMaxLength);
+
+            builder.Property(x => x.IsActive)
+                .IsRequired();
+
+            builder.HasOne(x => x.ProductCategory)
+                .WithMany(x => x.Products)
+                .HasForeignKey(x => x.ProductCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.DefaultMeasure)
+                .WithMany(x => x.DefaultProducts)
+                .HasForeignKey(x => x.DefaultMeasureId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(x => x.ProductMeasures)
+                .WithOne(x => x.Product)
+                .HasForeignKey(x => x.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.ProductNutrients)
+                .WithOne(x => x.Product)
+                .HasForeignKey<ProductNutrients>(x => x.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
